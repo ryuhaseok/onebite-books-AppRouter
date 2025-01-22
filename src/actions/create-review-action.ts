4aa82ea31d60faea5c70109f4336159c262a1ad5
ryuhaseok <rhs0402@naver.com>
2025-01-22 16:13:46 +0900

    클라이언트컴포넌트 서버액션, 리뷰 삭제 구현

--- src/actions/create-review-action.ts
+++ src/actions/create-review-action.ts
@@ -1,22 +1,32 @@
 "use server";
 
+import { delay } from "@/util/delay";
+import { error } from "console";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       { method: "POST", body: JSON.stringify({ bookId, content, author }) }
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     // 1. 특정 주소의 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
     // // 풀라우트캐시와 데이터캐시를 삭제하고 다시 데이터를 받아와 렌더링.
@@ -32,8 +42,14 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
